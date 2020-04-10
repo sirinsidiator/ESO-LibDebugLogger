583c76adb05b4728915f7bafd05aeeff23d7b109
sirinsidiator <insidiator@cmos.at>
2020-04-10 17:52:13 +0200

    improved startup debug logging

--- src/Initialization.lua
+++ src/Initialization.lua
@@ -7,18 +7,42 @@ local traceback = debug.traceback
 local zo_strformat = zo_strformat
 local GetGameTimeMilliseconds = GetGameTimeMilliseconds
 
+local UNKNOWN_STATE_STRING = "unknown state (%d)"
+local STATE_STRING = {
+    [ADDON_STATE_NO_STATE] = "no state",
+    [ADDON_STATE_TOC_LOADED] = "toc loaded",
+    [ADDON_STATE_ENABLED] = "enabled",
+    [ADDON_STATE_DISABLED] = "disabled",
+    [ADDON_STATE_VERSION_MISMATCH] = "out of date",
+    [ADDON_STATE_DEPENDENCIES_DISABLED] = "missing dependency",
+    [ADDON_STATE_ERROR_STATE_UNABLE_TO_LOAD] = "failed to load",
+}
+local PLATFORMS = {
+    [0] = "PC",
+    [1] = "Playstation",
+    [2] = "Xbox",
+    [3] = "PC - DMM",
+    [4] = "PC - Steam",
+    [5] = "Stadia"
+}
+
 -- player info logging
 
 local AddOnManager = GetAddOnManager()
 local numAddons = AddOnManager:GetNumAddOns()
 local numEnabledAddons = 0
 local addOnInfo = {}
+local skippedAddOnInfo = {}
 for i = 1, numAddons do
-    local name, _, _, _, enabled = AddOnManager:GetAddOnInfo(i)
+    local name, _, _, _, enabled, state = AddOnManager:GetAddOnInfo(i)
     local version = AddOnManager:GetAddOnVersion(i)
     local directory = AddOnManager:GetAddOnRootDirectoryPath(i)
     if(enabled) then
-        addOnInfo[name] = strformat("Addon loaded: %s, AddOnVersion: %d, directory: '%s'", name, version, directory)
+        if(state == ADDON_STATE_ENABLED) then
+            addOnInfo[name] = strformat("Addon loaded: %s, AddOnVersion: %d, directory: '%s'", name, version, directory)
+        else
+            skippedAddOnInfo[name] = strformat("Did not load addon: %s, AddOnVersion: %d, directory: '%s', state: %s", name, version, directory, STATE_STRING[state] or UNKNOWN_STATE_STRING:format(state))
+        end
         numEnabledAddons = numEnabledAddons + 1
     end
 end
@@ -29,8 +53,8 @@ local debugInfo = {
     internal.FormatTime(internal.SESSION_START_TIME),
     GetESOVersionString(),
     GetWorldName(),
-    GetString("SI_PLATFORMSERVICETYPE", GetPlatformServiceType()),
-    IsConsoleUI() and "gamepad" or "keyboard", -- TODO difference between this and IsInGamepadPreferredMode?
+    PLATFORMS[GetPlatformServiceType()],
+    IsInGamepadPreferredMode() and "gamepad" or "keyboard",
     IsESOPlusSubscriber() and "eso+" or "regular",
     GetCVar("language.2"),
     GetKeyboardLayout(),
@@ -87,6 +111,14 @@ EVENT_MANAGER:RegisterForEvent(lib.id, EVENT_PLAYER_ACTIVATED, function(event, i
     -- to fix this, we track if the event was the first of its kind and set the flag to false otherwise
     if(regularLoadingScreen) then
         initial = false
+    else
+        for name, message in pairs(addOnInfo) do
+            -- should not happen, but we want to know it in case it does
+            internal.Log(internal.LOG_LEVEL_WARNING, lib.id, "No loaded event detected for %s", name)
+        end
+        for name, message in pairs(skippedAddOnInfo) do
+            internal.Log(internal.LOG_LEVEL_WARNING, lib.id, message)
+        end
     end
 
     local now = internal.SESSION_START_TIME + GetGameTimeMilliseconds()
@@ -112,6 +144,7 @@ end)
 
 EVENT_MANAGER:RegisterForEvent(lib.id, EVENT_ADD_ON_LOADED, function(event, name)
     internal.Log(internal.LOG_LEVEL_INFO, lib.id, addOnInfo[name] or strformat("UI module loaded: %s", name))
+    addOnInfo[name] = nil
 
     if(name == lib.id) then
         -- CHAT_SYSTEM:AddMessage is actually not used for debugging anymore, but pChat still routes d() messages via this method. we'll hook it until pChat is updated
