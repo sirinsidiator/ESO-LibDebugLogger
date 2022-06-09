f6b99a086c0d542dca1fe73ada8caf53901f2a9d
sirinsidiator <insidiator@cmos.at>
2022-06-09 22:06:59 +0200

    renamed StartUpConfig.lua to StartUpConfig.example.lua and uncommented settings so users can simply rename the file to enable full logging

--- src/StartUpConfig.example.lua
+++ src/StartUpConfig.example.lua
@@ -3,11 +3,11 @@ local internal = lib.internal
 
 -- these changes are used during UI load and get discarded when the real settings are loaded,
 -- or if no settings have been created yet, they will be kept instead of the defaults.
---internal.settings.logTraces = true
---internal.settings.minLogLevel = internal.LOG_LEVEL_VERBOSE
+internal.settings.logTraces = true
+internal.settings.minLogLevel = internal.LOG_LEVEL_VERBOSE
 
 -- enable adding the stacktrace of the call to zo_callLater to the actual stacktrace
---internal.logOriginStacktrace = true
+internal.logOriginStacktrace = true
 
 -- add tags to the verbose whitelist below so they are logged during UI load.
 local whitelist = internal.verboseWhitelist
